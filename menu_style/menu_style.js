37e082fb99079b720e1c46cbd025d45e1be7a18a
Thomas Thune Hansen <tth@syddjurs.(none)>
2012-05-09 14:43:13 +0200

    tilrettelse af menu_style.js

--- menu_style/menu_style.js
+++ menu_style/menu_style.js
@@ -2,7 +2,9 @@
   // Last part is the internal name of the style.
   jQuery.fn.qtip.styles.cmstheme = {
   background-color: 'transparent',
-  padding: 0,
+  padding-left: 0,
+  padding-right: 0,
+  padding-bottom: 0,
  };
 })(jQuery);
 
