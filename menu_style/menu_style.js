13e54bef7a8de076e1338183bbc3596e5575d6d3
Thomas Thune Hansen <tth@syddjurs.(none)>
2012-05-09 15:26:39 +0200

    bla

--- menu_style/menu_style.js
+++ menu_style/menu_style.js
@@ -1,10 +1,8 @@
 (function($) {
   // Last part is the internal name of the style.
   jQuery.fn.qtip.styles.cmstheme = {
-  background-color: 'transparent',
-  padding: 0,
+  padding: '0px 0px', 
+  background: 'none',
+
  };
 })(jQuery);
-
-
-
