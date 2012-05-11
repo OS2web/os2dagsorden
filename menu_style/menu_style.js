ec42ada28596a14881403e2a6096e862eb67106b
Thomas Thune Hansen <tth@bellcom.dk>
2012-05-11 10:21:45 +0200

    Glemte filerne før menu_style.js + search.js

---
+++ menu_style/menu_style.js
@@ -0,0 +1,10 @@
+ (function($) {
+   // Last part is the internal name of the style.
+   jQuery.fn.qtip.styles.cmstheme = {
+	  	
+  padding-bottom: 0,
+  padding: '0px 0px', 
+  background: 'none',
+  };
+	
+ })(jQuery);
