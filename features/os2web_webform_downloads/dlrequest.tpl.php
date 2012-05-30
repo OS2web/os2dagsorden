40541519921d17493f7fb8d990d08d455d042e27
Jesper Mathiassen <jm@bellcom.dk>
2012-05-30 12:59:52 +0200

    Updates

--- features/os2web_webform_downloads/dlrequest.tpl.php
+++ features/os2web_webform_downloads/dlrequest.tpl.php
@@ -4,24 +4,20 @@
  * $name - Name as entered
  * $last_name - Last name as entered
  * $email - E-mail adress as entered
- * $filename - Filename from the webform node
- * $fileurl - Absolut URL to the file
+ * $files - Rendered field with file attachments
  * $node - Full node of the webform node
  */
 ?>
 <h2>Tak for din registrering!</h2>
 
 <p>Dine registrerede informaiton er:</p>
-<ul>
-<li>
-Fulde navn: <?=$name?> <?=$last_name?></li>
-<li>E-mail: <?=$email?></li>
-</ul>
+<?php if (count($fields)>1): ?>
+<p><ul class="webform-fields">
+<?php foreach ($fields as $field): ?>
+<li><?=$field['label']?>: <?=$field['value']?></li>
+<?php endforeach; ?> 
+</ul></p>
+<?php endif; ?>
 
 <p>De ønskede filer kan downloades her:</p>
 <?=$files?>
-<ul>
-<?php foreach ($files as $file):?>
-<li><?=$file?></li>
-<?php endforeach;?>
-</ul>
